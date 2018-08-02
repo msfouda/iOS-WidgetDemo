aa9a3ab754774a756e6f9f8f3405d8a67f05a3a6
mfouda214 <m.fouda@robone.net>
2018-08-02 22:34:42 +0100

    Add Embedded Framework

---
+++ QouatesNetworking/QouatesNetworking.h
@@ -0,0 +1,19 @@
+//
+//  QouatesNetworking.h
+//  QouatesNetworking
+//
+//  Created by Mohamed Sobhi  Fouda on 8/2/18.
+//  Copyright © 2018 CareerFoundry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QouatesNetworking.
+FOUNDATION_EXPORT double QouatesNetworkingVersionNumber;
+
+//! Project version string for QouatesNetworking.
+FOUNDATION_EXPORT const unsigned char QouatesNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QouatesNetworking/PublicHeader.h>
+
+
--- WidgetDemo/QuoteViewController.swift
+++ WidgetDemo/QuoteViewController.swift
@@ -8,6 +8,7 @@
 //
 
 import UIKit
+import QouatesNetworking
 
 class QuoteViewController: UIViewController {
 
